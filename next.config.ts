f5d0a95a58dad008a69a9bb413ed2e06b83f0160
osvaldosurbakti <osvaldosurbakti@gmail.com>
2025-03-09 17:45:33 +0700

    redeploy5?

--- next.config.ts
+++ next.config.ts
@@ -4,12 +4,7 @@ const nextConfig: NextConfig = {
   images: {
     domains: ['via.placeholder.com'], // Tambahkan domain yang diizinkan di sini
     unoptimized: false, // Pastikan ini *tidak* diubah ke true
-  },
-  rules: [
-    {
-      "react/no-unescaped-entities": "off"
-    }
-  ]
+  }
 };
 
 export default nextConfig;
